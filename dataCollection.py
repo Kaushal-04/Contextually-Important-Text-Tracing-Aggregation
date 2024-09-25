be53f1e5b7738a02d08eea2b440173f4779b2ced
Kaushal-04 <kaushaljee82@gmail.com>
2024-09-25 11:38:40 +0530

    Update the function

--- dataCollection.py
+++ dataCollection.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://en.wikipedia.org/wiki/News'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-paragraphs = soup.find_all('p')  
-texts = [paragraph.text for paragraph in paragraphs]
-print(texts)
+def dataCollect():
+    url = 'https://en.wikipedia.org/wiki/News'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    paragraphs = soup.find_all('p')  
+    texts = [paragraph.text for paragraph in paragraphs]
+    return texts
