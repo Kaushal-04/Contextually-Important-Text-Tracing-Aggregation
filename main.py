dde764f66bec29470735c25dba9a4f8b45845244
Kaushal-04 <kaushaljee82@gmail.com>
2024-10-09 17:30:11 +0530

    Update in the main function

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from dataCollection import dataCollect
 from dataPreprocessing import cleanText
+from ner import extract_entities
+from vectorize import vectorize_text
+from aggregate import aggregate_entities
+from plot import plot_aggregated_entities
 
+def main():
+    text_data = dataCollect()
 
-data = dataCollect()
+    print("Preprocessing text : ")
+    tokens = cleanText(text_data)
+    print(f"Tokens: {tokens}\n")
 
-texts = [cleanText(text) for text in data]
-print(texts)
+    print("Extracting entities : ")
+    entities = extract_entities(text_data)
+    print(f"Entities: {entities}\n")
+
+    print("Vectorizing text : ")
+    df = vectorize_text(text_data)
+    print("TF-IDF Vectorized DataFrame:")
+    print(df, "\n")
+
+    print("Aggregating entities : ")
+    aggregated_entities = aggregate_entities(entities)
+    print(f"Aggregated Entities: {aggregated_entities}\n")
+
+    print("Plotting aggregated entities : ")
+    plot_aggregated_entities(aggregated_entities)
+
+if __name__ == "__main__":
+    main()
