3d0db29b9f60cf8b596b5c5290393421889f460a
Kaushal-04 <kaushaljee82@gmail.com>
2024-10-09 16:41:29 +0530

    Update in data colletion send Hand coded text

--- dataCollection.py
+++ dataCollection.py
@@ -2,9 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 def dataCollect():
+    '''
     url = 'https://en.wikipedia.org/wiki/News'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     paragraphs = soup.find_all('p')  
     texts = [paragraph.text for paragraph in paragraphs]
+    '''
+    texts = """
+    Apple Inc. is looking at buying U.K. startup for $1 billion. 
+    The deal would mark the company's first acquisition in Europe. 
+    Tim Cook, CEO of Apple, stated that this acquisition is part of the company's strategy to expand its presence in the region.
+    """
     return texts
