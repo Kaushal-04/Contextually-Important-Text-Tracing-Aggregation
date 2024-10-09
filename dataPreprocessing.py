b1a997430bba0032db883494cf8de137467fe807
Kaushal-04 <kaushaljee82@gmail.com>
2024-10-09 16:52:21 +0530

    Update in the data preprocessing

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -1,8 +1,6 @@
-import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 import re
-import string
 
 # Uncomment this line to download stop words if not already downloaded
 #nltk.download('stopwords')
@@ -13,11 +11,9 @@ stop_words = set(stopwords.words('english'))
 def cleanText(texts):
     texts = re.sub(r'http\S+|www\S+|https\S+', '', texts, flags=re.MULTILINE)
     texts = re.sub(r'\d+', '', texts)
+    texts = re.sub(r'[^\w\s]', '', texts)  # Remove punctuation
     texts = re.sub(r'\s+', ' ', texts).strip()
-    texts = texts.translate(str.maketrans('', '', string.punctuation))
-    # Tokenize the text
-    words = word_tokenize(texts)
-    # Remove stop words
-    texts = ' '.join(word for word in words if word not in stop_words)
     texts = texts.lower()
-    return texts
+    tokens = word_tokenize(texts)
+    tokens = [word for word in tokens if word not in stop_words] # Remove stop words
+    return tokens
