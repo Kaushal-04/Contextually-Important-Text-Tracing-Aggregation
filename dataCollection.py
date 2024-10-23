e1f91a0f7c2304a45011c60ccd231c83b66132e3
Kaushal Kumar <kaushaljee82@gmail.com>
2024-10-23 11:11:23 +0530

    Update in dataCollection.py

--- dataCollection.py
+++ dataCollection.py
@@ -1,18 +1,14 @@
-#import requests
-#from bs4 import BeautifulSoup
-
 def dataCollect():
-    '''
-    url = 'https://en.wikipedia.org/wiki/News'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    paragraphs = soup.find_all('p')  
-    texts = [paragraph.text for paragraph in paragraphs]
-    '''
-    texts = """
-    Apple Inc. is looking at buying U.K. startup for $1 billion. 
-    The deal would mark the company's first acquisition in Europe. 
-    Tim Cook, CEO of Apple, stated that this acquisition is part of the company's strategy to expand its presence in the region.
+    texts = """Ratan Tata was born in Bombay (now Mumbai), during the British Raj, into a Parsi Zoroastrian family, 
+    on 28 December 1937.[10] He was the son of Naval Tata (who was born in Surat and later adopted into the Tata family), 
+    and Soonoo Tata (the niece of Tata group founder Jamsetji Tata). Tata's biological grandfather, Hormusji Tata was a member 
+    of the Tata family by blood. In 1948, when Tata was 10, his parents separated, and he was subsequently raised and adopted by 
+    Navajbai Tata, his grandmother and widow of Ratanji Tata.[11] He had a younger brother Jimmy Tata[12] and a half-brother, Noel 
+    Tata, from Naval Tata's second marriage to his stepmother Simone Tata.
+    Tata studied at the Campion School, Mumbai until 8th grade. He then continued his studies at the Cathedral and John Connon School 
+    in Mumbai, the Bishop Cotton School in Shimla, and the Riverdale Country School in New York City, from which he graduated in 1955.
+    [13][14][15] After high school, Tata enrolled in Cornell University, from which he graduated with a bachelor's degree in 
+    architecture in 1962.[16][17] While at Cornell, Tata became a member of the Alpha Sigma Phi Fraternity. In 2008, Tata gifted 
+    Cornell $50 million, becoming the largest international donor in the university's history.[18][19]
     """
-    
     return texts
