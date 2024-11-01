53518f313c23b1f821b2f6575a399e39fcde1333
Kaushal Kumar <kaushaljee82@gmail.com>
2024-11-01 11:30:13 +0530

    Update in dataPreProcessing

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -2,19 +2,26 @@ import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 import re
+from dataCollection import dataCollect
 
 # Uncomment this line to download stop words if not already downloaded
-#nltk.download('stopwords')
-#nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('punkt')
 
 stop_words = set(stopwords.words('english'))
 
 def cleanText(texts):
+    # Remove URLs, numbers, and punctuation
     texts = re.sub(r'http\S+|www\S+|https\S+', '', texts, flags=re.MULTILINE)
     texts = re.sub(r'\d+', '', texts)
     texts = re.sub(r'[^\w\s]', '', texts)  # Remove punctuation
     texts = re.sub(r'\s+', ' ', texts).strip()
     texts = texts.lower()
+    
+    # Tokenize and remove stop words
     tokens = word_tokenize(texts)
-    tokens = [word for word in tokens if word not in stop_words] # Remove stop words
-    return tokens
+    tokens = [word for word in tokens if word not in stop_words]
+    
+    # Join tokens back into a single sentence
+    cleaned_sentence = ' '.join(tokens)
+    return cleaned_sentence
