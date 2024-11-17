5448f25e4b90c8d667690beda785e8dfbf176664
Kaushal Kumar <kaushaljee82@gmail.com>
2024-11-17 06:59:20 +0530

    Add openAi API

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template, request, jsonify
 from dataPreprocessing import process_text
 from matchMatrix import create_match_matrix
 from pairGeneration import generate_pairs
+import openai
+import os
 
 app = Flask(__name__)
+
+api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("API Key is not set. Please set the OPENAI_API_KEY environment variable.")
+
+openai.api_key = api_key
 
 # Route for the dashboard (homepage)
 @app.route('/')
