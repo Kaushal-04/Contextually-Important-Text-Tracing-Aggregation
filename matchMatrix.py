f691e8c2f38e2068a440e7168fb51bd13410bfb4
Kaushal Kumar <kaushaljee82@gmail.com>
2025-02-21 17:34:37 +0530

    Revert "file added"
    This reverts commit ca2c066cbd0035c253c8859afab47fbcc54095bf.

--- matchMatrix.py
+++ matchMatrix.py
@@ -5,9 +5,6 @@ from nltk import pos_tag
 # Download required NLTK resources
 #nltk.download('punkt')
 #nltk.download('averaged_perceptron_tagger')
-#nltk.download('punkt_tab')
-#nltk.download('wordnet')
-#nltk.download('averaged_perceptron_tagger_eng')
 
 def get_nouns(sentence):
     """Extract nouns from a given sentence."""
